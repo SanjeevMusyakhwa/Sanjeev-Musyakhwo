4c31462e6ce7185445e6d3828205c79988fe6ea8
SanjeevMusyakhwa <sanjeevmusyakhwa@gmail.com>
2024-12-20 09:48:10 +0545

    changes made

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,18 @@
-var typed2 = new Typed('.text', {
-  strings: ['Developer-->', 'Freelancer-->', 'Designner-->'],
+var typed1 = new Typed('.text', {
+  strings: ['Developer-->', 'Freelancer-->', 'Designer-->'],
   typeSpeed: 150,      // Speed of typing
+  backSpeed: 150,      // Speed of backspacing
+  backDelay: 100,      // Delay before backspacing starts
+  cursorChar: '_',     // Cursor character
+  loop: true,          // Loop forever
+});
+
+var typed2 = new Typed('.hero_text', {
+  strings: ['Developer-->', 'Freelancer-->','Designer-->',],
+  typeSpeed: 100,  
+      // Speed of typing
   backSpeed: 100,      // Speed of backspacing
-  backDelay: 1000,     // Delay before backspacing starts
+  backDelay:100,  // Delay before backspacing starts
   cursorChar: '_',     // Cursor character
   loop: true,          // Loop forever
 });
